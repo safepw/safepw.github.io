574b8e9151110e22eab367e87cfb9f06aa7aac6f
L5on <78597735+L5on@users.noreply.github.com>
2021-06-09 15:46:17 +0200

    Update countDownDate.js

--- js/countDownDate.js
+++ js/countDownDate.js
@@ -15,9 +15,9 @@ var x = setInterval(function() {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60 )) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+    
+    title.innerHTML = 'Create a SAFE Passwort in ' + days + 'd ' + hours + 'h ' + minutes + 'm ';
     launch.innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
-    title.innerHTML = 'Create a SAFE Passwort in' + days + 'd ' + hours + 'h ' + minutes + 'm ';
 
     if(distance < 0){
         clearInterval(x)
