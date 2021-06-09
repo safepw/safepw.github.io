fa5793d8331670ebc68a74a6e260f84a990c2cf2
L5on <78597735+L5on@users.noreply.github.com>
2021-06-09 15:42:29 +0200

    Update countDownDate.js

--- js/countDownDate.js
+++ js/countDownDate.js
@@ -1,6 +1,7 @@
 const launch = function countDownDate(){
 
 var launch = document.getElementById('launch')
+var title = document.getElementById('title')
 
 var countDownDate = new Date('Jun 19, 2021 23:59').getTime()
 
@@ -16,10 +17,12 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     launch.innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
+    launch.innerHTML = 'Create a SAFE Passwort days in' + days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
 
     if(distance < 0){
         clearInterval(x)
-        launch.innerHTML(`EXPIRED`)
+        title.innerHTML(`Create a SAFE passwort now | safepw`)
+        launch.innerHTML(`Create a SAFE passwort now on <a href="https://safepw.github.io/create-pw/">safepw.github.io/create-pw/</a>`)
         window.location = "https://safepw.github.io/create-pw/";
     }
 
